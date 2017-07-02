8f6d7406df3b67f299588c5ff51150d52ab55906
Niki Vazou <nikivazou@gmail.com>
2017-07-02 18:16:14 +0300

    Create IntInduction.hs

---
+++ examples/IntInduction.hs
@@ -0,0 +1,10 @@
+import Language.Haskell.Liquid.ProofCombinators
+
+{-@ LIQUID "--higherorder" @-}
+
+{-@ natinduction :: p:(Int-> Bool) -> PAnd {v:Proof | p 0} (n:Int -> {v:Proof | p (n-1)} -> {v:Proof | p n})
+                 -> n:Nat -> {v:Proof | p n} / [n]  @-}
+natinduction :: (Int-> Bool) -> PAnd Proof (Int -> Proof -> Proof)-> Int -> Proof 
+natinduction p (PAnd p0 pi) n  
+  | n == 0    = p0 
+  | otherwise = pi n (natinduction p (PAnd p0 pi) (n-1))
