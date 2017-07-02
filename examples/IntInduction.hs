a820e15aecafa07debc0b503b557cb2f82ffd05d
Niki Vazou <nikivazou@gmail.com>
2017-07-03 01:58:11 +0300

    Update IntInduction.hs

--- examples/IntInduction.hs
+++ examples/IntInduction.hs
@@ -3,7 +3,7 @@ import Language.Haskell.Liquid.ProofCombinators
 {-@ LIQUID "--higherorder" @-}
 
 {-@ natinduction :: p:(Int-> Bool) -> PAnd {v:Proof | p 0} (n:Int -> {v:Proof | p (n-1)} -> {v:Proof | p n})
-                 -> n:Nat -> {v:Proof | p n} / [n]  @-}
+                 -> n:Nat -> {v:Proof | p n}  @-}
 natinduction :: (Int-> Bool) -> PAnd Proof (Int -> Proof -> Proof)-> Int -> Proof 
 natinduction p (PAnd p0 pi) n  
   | n == 0    = p0 
